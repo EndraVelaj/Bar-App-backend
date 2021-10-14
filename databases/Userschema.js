c696f9edc564677ae6154874889e596c29f3f7ee
EndraVelaj <88143746+EndraVelaj@users.noreply.github.com>
2021-10-14 17:40:09 +0200

    Add files via upload

--- databases/Userschema.js
+++ databases/Userschema.js
@@ -3,8 +3,8 @@ const  mongoose = require("mongoose");
 
 const userSchema =  mongoose.Schema({
     username:  { type: String, required: true },
-    email : { type : String , required : true , unique : true},
-    password:  { type: String, required: true },
+    email : { type : String , required : true , unique : true, trim:true},
+    password:  { type: String, required: true, trim:true },
     isAdmin : { type : Number, required : false ,default : 1 }
   });
 
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require ("express");
 const app = express();
-const router = require("./router");
 const port= 8080;
-const uri = "mongodb+srv://endra:endra@cluster0.zqqz7.mongodb.net/bar-app-2?retryWrites=true&w=majority";
+const uri = "mongodb+srv://endra:endra@cluster0.gujrt.mongodb.net/bar_app?retryWrites=true&w=majority";
 const cors = require('cors');
 const mongoose = require("mongoose");
 const userModel = require ("./databases/Userschema");
 const ProductModel = require("./databases/ProductSchema");
+require('dotenv').config()
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
@@ -19,55 +19,79 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.get('/save-user', (req, res) => {
-//     res.sendFile();
-//   });
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-app.post('/register-user', (req, res) => {
-    const userTable = new userModel({
-         username : req.body.username,
-         email : req.body.email,
-         password : req.body.password,
-         isAdmin : req.body.isAdmin
-    });
 
-    userTable.save()
-    .then((result)=>{
-        console.log("result in backend-----",result);
-        return res.json({ code: 200, message: "user data", result: result })
-    })
-    .catch((error)=>{
-        return res.json({ code: 500, message: "un error has ocurred", error: error })
-    })
-  });
+app.post('/register-user', async (req, res) => {
+    try{
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        console.log( salt)
+        console.log(hashedPassword)
+
+        const userTable = new userModel ({ username : req.body.username, email : req.body.email, password : hashedPassword });
+
+        userTable.save()
+            .then((result)=>{
+                console.log("result in backend-----",result);
+                return res.json({ code: 200, message: "user data", result: result })
+            })
+    } catch {
+        res.status(500).send()
+    }
+}); 
+
 
 app.post('/login-user', async(req,res) =>{
 
-    console.log("login-user----" , req.body);
     const checkUser = await userModel.findOne({email: req.body.email});
-
-   // console.log("checkUser----" , checkUser);
-
-    //checkUser = checkUser ? checkUser : null;
-    if(checkUser){
-        console.log("checkUser-----",checkUser);
-        return res.json({ code: 200, message: "user is registered", result: checkUser }) 
-
-    } else {
-        return res.json({ code: 404, message: "user is not registered", result: null })
+    if(checkUser == null) {
+       return res.json({ code: 404, message: "user is not registered", result: null })
     }
-
+    try {
+       if (await bcrypt.compare(req.body.password, checkUser.password)) {
+        const accessToken = jwt.sign({name:checkUser.name}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30m'})
+        return res.json({ code: 200, message: 'Success!', token: accessToken })
+       }
+       else {
+        return res.json({ code: 404, message: 'Wrong email/password!'})
+         }
+       } catch {
+        return res.json({ code: 500, message: "An internal server error ", error: error })
+    }
 });
+    
+
+
+//    try{
+
+//     console.log("login-user----" , req.body);
+//     const checkUser = await userModel.findOne({email: req.body.email});
+
+//     if (checkUser) {
+//         const accessToken = jwt.sign({name:checkUser.name}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30m'})
+//         res.json({ token: accessToken, result:  })
+//     } else {
+//         res.json({ code: 404, message: "user is not registered", result: null })
+//     }
+//    } catch(error){
+//         console.log("error-----", error);
+//      res.json({ code: 500, message: "un internal server error ", error: error })
+//    }
+// });
+
+
 
 app.post("/save-product", (req, res) => {
 
     console.log("save-product-------",req.body)
 
     const product = new ProductModel({
-        name: req.body.p_name,
-        price: req.body.p_price,
-        amount: req.body.p_amount,
-        total: req.body.p_total
+        name: req.body.name,
+        price: req.body.price,
+        amount: req.body.amount,
+        total: req.body.total
     });
 
     product.save()
